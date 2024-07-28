1dc4b4703697524ecb1ee1a571aab273cfa3568b
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-07-28 12:55:41 +0530

    added list output converter

---
+++ src/main/java/com/practice/ai/prompts/SongsController.java
@@ -0,0 +1,43 @@
+package com.practice.ai.prompts;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.converter.ListOutputConverter;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class SongsController {
+
+    private final ChatClient chatClient;
+
+    public SongsController(ChatModel chatModel) {
+        this.chatClient = ChatClient.builder(chatModel)
+                .defaultAdvisors(new PromptChatMemoryAdvisor(new InMemoryChatMemory())) // advisor -> intercepts the request and modifies it. this is used to preserve the state of request
+                .build();
+    }
+
+    @GetMapping("/songs")
+    public List<String> getSongsByArtist(@RequestParam(value = "artist", defaultValue = "Taylor Swift") String artist) {
+        String message = """
+                Please give me top 10 songs of artist {artist}.\s
+                """;
+        // for getting list in response
+        ListOutputConverter listOutputConverter = new ListOutputConverter(new DefaultConversionService());
+
+        PromptTemplate promptTemplate = new PromptTemplate(message, Map.of("artist", artist) "format", listOutputConverter.getFormat());
+        Prompt prompt = promptTemplate.create();
+        return listOutputConverter.convert(chatClient.prompt(prompt)
+                .call()
+                .content());
+    }
+}
